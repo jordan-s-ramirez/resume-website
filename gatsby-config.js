33bfa0f29665cf50ef7ec33394eb9f41834db004
jordan-s-ramirez <jordan.s.ramirez.com>
2023-01-30 14:10:26 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,19 +9,49 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Resume Website`,
+    description: `Static website, with centrally located assets`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `portfolio`,
+        path: `${__dirname}/src/assets/portfolio`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1000,
+              quality: 80,
+              showCaptions: true,
+              linkImagesToOriginal: false,
+            },
+          }
+        ]
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
---
+++ src/assets/portfolio/FightPokemon/project.md
@@ -0,0 +1,15 @@
+---
+title: Fight Trainers
+date: 2019-11-11T05:25:44.226Z
+path: portfolio/FightPokemon
+cover: ./battle.png
+excerpt: Designing and building an audio equalizer
+currData: "## Inital Design\n
+[] FightPokemon/battle.png\n
+However, there are many tricks you can use to try to increase your chance of better results. More battle wins mean more rewards, so they are definitely worth mastering. Plus, who doesn't want to flex their well-raised Pokemon. With these tips, you'll be a battle league expert in no time!Updated December 17, 2021 by Gabrielle Huston: Pokemon GO has managed to maintain a fanbase despite controversy over how the service has been handled during the pandemic. Trainer battles can be daunting, even for experienced players. There are a lot of stats (not to mention grinding) that go into it becoming the best of the best. You don't need that just to compete, though! If you're looking to get started and become familiar with the process, look no further than these easy tips.\n
+Changes Made:\n
+* Swapped Pokmon\n
+* Train Pokemon\n
+"
+
+---
